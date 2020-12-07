b96d15119e3439518bbded0163c6d73bf02fb8a5
Zack Palmer <zacnoo@gmail.com>
2020-12-07 18:55:37 +1300

    Added some onmouseover stuff

--- zacnoo.nz/custom.js
+++ zacnoo.nz/custom.js
@@ -11,7 +11,47 @@ $(document).ready(function() {
 			translateY: 50 + (50 * index),
 			backgroundColor: "#F60",
 			color: "#151515",
-			delay: 200 + (200 * index)
+			delay: 1000 + (200 * index)
 		});
 	});
 });
+
+function moveDownwards(element){
+	element.parentElement.appendChild(element.cloneNode(true))
+	var currentTranlate = getTranslateXY(element);
+		var animation = anime({
+			targets:element,
+			translateY:function(){
+				var translation = Math.random()*300;
+				if(currentTranlate.translateY > translation){
+					return 0;
+				}else{
+					return translation;
+				}
+			},
+			rotate:Math.random()*360,
+			translateX:function(){
+				var translation = Math.random()*300;
+				if(currentTranlate.translateY > translation){
+					return translation;
+				}else{
+					return 0;
+				}
+			},
+			rotate:Math.random()*360,
+			duration:10,
+			easing:'easeOutQuad',
+			changeComplete:function(){
+				moveDownwards(element);
+			}
+		})
+	}
+
+function getTranslateXY(element) {
+    const style = window.getComputedStyle(element)
+    const matrix = new DOMMatrixReadOnly(style.transform)
+    return {
+        translateX: matrix.m41,
+        translateY: matrix.m42
+    }
+}
