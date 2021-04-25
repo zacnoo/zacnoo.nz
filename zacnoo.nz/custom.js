826d068e9d16be89cd14c3ca0e3b42b062f60dfe
Zack Palmer <zacnoo@zacks-air.local>
2021-04-25 22:56:44 +1200

    Added Oculus link issue.
    Removed old junk, added home placeholder.

--- zacnoo.nz/custom.js
+++ zacnoo.nz/custom.js
@@ -1,57 +1,35 @@
 WebFont.load({
-	google: {
-		families: ['Poppins', 'Nunito Sans']
-	}
-});
-
-$(document).ready(function() {
-	$('.staggering').each(function(index, el) {
-		anime({
-			targets: this,
-			translateY: 50 + (50 * index),
-			backgroundColor: "#F60",
-			color: "#151515",
-			delay: 1000 + (200 * index)
-		});
-	});
-});
-
-function moveDownwards(element){
-	element.parentElement.appendChild(element.cloneNode(true))
-	var currentTranlate = getTranslateXY(element);
-		var animation = anime({
-			targets:element,
-			translateY:function(){
-				var translation = Math.random()*300;
-				if(currentTranlate.translateY > translation){
-					return 0;
-				}else{
-					return translation;
-				}
-			},
-			rotate:Math.random()*360,
-			translateX:function(){
-				var translation = Math.random()*300;
-				if(currentTranlate.translateY > translation){
-					return translation;
-				}else{
-					return 0;
-				}
-			},
-			rotate:Math.random()*360,
-			duration:10,
-			easing:'easeOutQuad',
-			changeComplete:function(){
-				moveDownwards(element);
-			}
-		})
-	}
-
-function getTranslateXY(element) {
-    const style = window.getComputedStyle(element)
-    const matrix = new DOMMatrixReadOnly(style.transform)
-    return {
-        translateX: matrix.m41,
-        translateY: matrix.m42
+    google: {
+        families: ['Poppins', 'Nunito Sans']
     }
+});
+var currentTime = 0;
+var delay = 0
+var rof = 400
+$(document).ready(function() {
+startAnimations()
+});
+
+var startAnimations = function() {
+    anime.remove('#actualinput')
+    anime({
+        targets: '#actualinput',
+        width: ['100%', '0%'],
+        loop: true,
+        direction: 'alternate',
+        easing: 'linear',
+        duration: rof,
+        delay: delay,
+        update: function(anim) {
+            anime({
+                targets: '#gameinput',
+                width: [$('#gameinput').width(), $('#actualinput').width()],
+                easing: 'linear',
+                duration: '50ms',
+                update: function(anime) {
+                	console.log($('#gameinput').width())
+                }
+            })
+        }
+    })
 }
